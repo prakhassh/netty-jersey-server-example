e9800b86c47a74bd96aeff384826941a6b1242f6
Prakhassh Yuvaraja <appu@Prakhassh-Yuvarajas-Mac.local>
2018-11-15 22:34:08 -0600

    for git flow

--- src/main/java/pl/druminski/netty/jersey/HelloNettyEndpoint.java
+++ src/main/java/pl/druminski/netty/jersey/HelloNettyEndpoint.java
@@ -1,16 +1,19 @@
 package pl.druminski.netty.jersey;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.xml.ws.Response;
 
 @Path("/hellonetty")
 public class HelloNettyEndpoint {
 
     @GET
+    @Path("/get")
     @Produces("text/plain")
     public String helloNetty() {
-        return "Hello Netty";
-    }
+        return "Hello Netty"; }
+        
 
 }
